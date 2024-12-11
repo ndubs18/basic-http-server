7626c2122eae37c4ad04d847a62a8426ddf8d0be
ndubs18 <nick.winkler1@gmail.com>
2024-12-11 10:59:11 -0800

    added buffer functions and function to cut message

--- http-server.ts
+++ http-server.ts
@@ -9,6 +9,43 @@ import * as net from 'net'
 
 const host: string = '127.0.0.1';
 const port: number = 1234;
+const kMaxHeaderLen = 1024 * 8;
+
+/* types */
+
+type TCPConn = {
+	socket: net.Socket,
+	err: null | Error,
+	ended: boolean,
+	//object holding executer function callbacks for promises
+	reader: null | {
+		resolve: (value: Buffer) => void,
+		reject: (reason: Error) => void
+	}
+}
+
+type HTTPReq = {
+	method: string,
+	uri: Buffer,
+	version: string,
+	headers: Buffer[]
+}
+
+type BodyReader = {
+	length: number,
+	read: () => Promise<Buffer>,
+}
+
+type HTTPRes = {
+	code: number;
+	headers: Buffer[];
+	body: BodyReader;
+}
+
+type DynBuf = {
+	data: Buffer,
+	length: number,
+}
 
 let soInit = (socket: net.Socket): TCPConn => {
 	//create our connection object
@@ -46,10 +83,55 @@ let soInit = (socket: net.Socket): TCPConn => {
 
 }
 
+let bufPush = (buf: DynBuf, data: Buffer): void => {
+
+	const newLen = buf.length + data.length;
+
+	if (buf.data.length < newLen) {
+
+		let cap = Math.max(buf.data.length, 32);
+
+		while (cap < newLen) {
+
+			cap *= 2;
+
+		}
+		const grown = Buffer.alloc(cap);
+		buf.data.copy(grown, 0, 0);
+		buf.data = grown;
+	}
+
+	data.copy(buf.data, buf.length, 0);
+	buf.length = newLen;
+
+}
+// TODO: We need to understand this function more
+let bufPop = (buf: DynBuf, len: number): void => {
+	buf.data.copyWithin(0, len, buf.length);
+	buf.length -= len;
+}
+
+let cutMessage = (buf: DynBuf): null | Buffer => {
+	// end of http header is delimited by \r\n\r\n
+	const idx = buf.data.subarray(0, buf.length).indexOf('\r\n\r\n');
+
+	if (idx < 0) {
+		if (buf.length >= kMaxHeaderLen) {
+			// TODO: We need to figure out what to do about our own error type
+			//throw new HTTPError(413, 'header is too large');
+		}
+		return null;
+	}
+
+
+
+}
+
 let serveClient = async (socket: net.Socket) => {
 	//initialize the net.Socket object with event listeners
 	const conn: TCPConn = soInit(socket);
 
+	const buf: DynBuf = { data: Buffer.alloc(0), length: 0 }
 	while (true) {
 		const data = await soRead(conn);
 		//the client closes the connection
@@ -73,19 +155,6 @@ let newConn = async (socket: net.Socket): Promise<void> => {
 		socket.destroy();
 	}
 }
-
-
-type TCPConn = {
-	socket: net.Socket,
-	err: null | Error,
-	ended: boolean,
-	//object holding executer function callbacks for promises
-	reader: null | {
-		resolve: (value: Buffer) => void,
-		reject: (reason: Error) => void
-	}
-}
-
 
 let soRead = (conn: TCPConn): Promise<Buffer> => {
 	//ensure this is the only read happening
